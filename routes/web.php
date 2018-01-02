e399cc5b4bbc0d085a6658222e085aeb0e5f6c90
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-02 19:25:42 +0100

    dodana naslovna stranica

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', 'PagesController@index');
 
 Route::get('/about', function() {
     return view('pages.about');
