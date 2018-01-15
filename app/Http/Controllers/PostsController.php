10e45cb01f1e855592d55f4ab5428f011a6626ea
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-15 13:33:22 +0100

    dodana funkcionalnost za brisanje

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -108,6 +108,9 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect('/posts')->with('success', 'Post Removed');
     }
 }
